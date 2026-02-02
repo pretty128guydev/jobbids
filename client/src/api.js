0b117db8be6876dcedbcc078da0dcdbbb7caa65b
pretty128guydev <night.fury.1208.0508@gmail.com>
2026-02-03 01:08:22 +0300

    update the backend api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4001/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://jobbids-ncob.onrender.com/api'
 });
 
 // send cookies for session-based auth
